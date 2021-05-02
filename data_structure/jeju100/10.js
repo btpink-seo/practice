e3f6aaa2910ca36306b6be3b7c1354e529d8d507
btpink-seo <btpink.seo@gmail.com>
2021-05-02 22:37:42 +0900

    세미콜론

--- data_structure/jeju100/10.js
+++ data_structure/jeju100/10.js
@@ -1,9 +1,10 @@
 const chrismas = (num) => {
   for (let i = 0; i < num; i++) {
-    const line = Array.from({ length: 4 - i }, () => ' ').concat(Array.from({ length: i }, () => '*'))
+    const line = Array.from({ length: num - 1 - i }, () => ' ').concat(Array.from({ length: i }, () => '*'));
     const reverseLine = line.slice().reverse();
     console.log(line.concat('*').concat(reverseLine).join(''));
   }
 };
 
 chrismas(5);
+chrismas(10);
