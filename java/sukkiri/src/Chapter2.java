c8dbcdb6d15b3d2745a423a694d7f17da4e887f7
btpink-seo <btpink.seo@gmail.com>
2021-08-29 18:09:48 +0900

    chang dir

--- java/sukkiri/src/Chapter2.java
+++ java/sukkiri/src/Chapter2.java
@@ -1,18 +1,17 @@
 import java.util.*;
-import java.lang.System.out.println;
 
 public class Chapter2 {
     public static void exec() {
         Scanner scanner = new Scanner(System.in);
-        println("Welcome uranai");
-        println("Insert your name");
+        System.out.println("Welcome uranai");
+        System.out.println("Insert your name");
         String name = scanner.nextLine();
-        println("Insert your age");
+        System.out.println("Insert your age");
         String ageString = scanner.nextLine();
         int fortune = new Random().nextInt(4);
         fortune++;
 
-        println("result : " + ageString + "_" + name + "_" + fortune);
+        System.out.println("result : " + ageString + "_" + name + "_" + fortune);
         scanner.close();
     }
 }
