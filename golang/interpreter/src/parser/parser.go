97a0907163b9b3e6a1b514cd58ee8cf040cde03e
btpink-seo <btpink.seo@gmail.com>
2021-09-26 21:50:49 +0900

    parser return

--- golang/interpreter/src/parser/parser.go
+++ golang/interpreter/src/parser/parser.go
@@ -62,9 +62,23 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
+}
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: 세미콜론을 만날 때까지 표현식을 건너뛴다.
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
