576f70dd36c8ea23ece2d099ab4c158f9eaf6755
btpink-seo <btpink.seo@gmail.com>
2021-09-26 21:21:15 +0900

    parse let

---
+++ golang/interpreter/src/parser/parser.go
@@ -0,0 +1,110 @@
+package parser
+
+import (
+	"fmt"
+	"monkey/src/ast"
+	"monkey/src/lexer"
+	"monkey/src/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer // 현재의 렉서 인스턴스
+	curToken  token.Token  // 현재의 토큰
+	peekToken token.Token  // 다음 토큰
+	errors    []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	// 토큰을 두 개 읽어서, curToken과 peekToken을 세팅
+	p.nextToken() // peekToken에 첫번째 토큰 등록
+	p.nextToken() // curToken에 첫번째 토큰, peekToken에 두번째 토큰 등록
+
+	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: 세미콜론을 만날 때 까지 표현식을 건너뛴다.
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// 현재 토큰과 같은지 비교
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// 다음 토큰과 같은지 비교
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// 인자가 다음 토큰과 같다면 한칸 진행 하고 true 아니면 false
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
